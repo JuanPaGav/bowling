4c3c3f25ca92ebefaa2f235016f6eafd669b61a0
Juan Pablo Gavidia <jpgavidiaa@gmail.com>
2026-02-21 17:27:19 -0600

    Ejercicio completado

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,3 +1,16 @@
 defmodule Bowling do
 
+  def score([[10,10,10]]), do: 30
+  #1
+  def score([]), do: 0
+  #3
+  def score([[f ,s | _] | [ff | rest]]) when f + s == 10, do: 10 + hd(ff) + score([ff]++rest)
+  #4
+  def score([[10,nil | _] | rest]) when hd(hd(rest)) != 10, do: 10 + hd(hd(rest)) + hd(tl(hd(rest))) + score(rest)
+  #5
+  def score([[10,nil | _] | rest]) when hd(tl(hd(rest))) != nil, do: 10 + hd(hd(rest)) + hd(tl(hd(rest))) + score(rest)
+  def score([[10,nil | _] | rest]), do: 10 + hd(hd(rest)) + hd(hd(tl(rest))) + score(rest)
+  #2
+  def score([[f ,s | _] | rest]), do: f + s + score(rest)
+
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -1,6 +1,31 @@
 defmodule BowlingTest do
+  use ExUnit.Case
 
-  test "greets the world" do
-    assert Bowling.hello() == :world
+  # 1. Gutter game
+  test "gutter game" do
+    game = List.duplicate([0, 0], 9) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 0
+  end
+
+  # 2. All ones game
+  test "'all ones' game" do
+    game = List.duplicate([1,1],9) ++ [[1,1,nil]]
+    assert Bowling.score(game) == 20
+  end
+
+  # 3. Game with one spare
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0,0],7) ++ [[0,0,nil]]
+    assert Bowling.score(game) == 16
+  end
+
+  test "one strike" do
+    game = [[10,nil],[3,4]] ++ List.duplicate([0,0],7) ++ [[0,0,nil]]
+    assert Bowling.score(game) == 24
+  end
+
+  test "perfect game" do
+    game = List.duplicate([10,nil],9) ++ [[10,10,10]]
+    assert Bowling.score(game) == 300
   end
 end
